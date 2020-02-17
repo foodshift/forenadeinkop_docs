088d97adfd0b11d7c41aae943211c40931b66f34
Kristofer Lund <kristofer@fmckl.se>
2020-02-17 09:57:15 +0100

    First commit

---
+++ doczrc.js
@@ -0,0 +1,20 @@
+// doczrc.js
+
+export default {
+  title: 'Förenade Inköp – Handbok',
+  description: 'Vad är en inköpsförening? Hur startar man och driver? Hur gör andra? Handboken samlar fakta och innehåller exempel.',
+  src: 'src',
+  // dest: '.docz/dist',
+  // typescript: true,
+  // propsParser: false,
+  // themeConfig: {
+  //   // mode: 'dark',
+  //   colors: {
+  //     primary: 'tomato',
+  //     secondary: 'khaki',
+  //     gray: 'lightslategray',
+  //   },
+  // },
+  // plugins: [
+  // ]
+}
